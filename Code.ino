cc4cf0f784cdc2090cbbcab1ba0a583f4b8e9c39
jylee1429 <123356726+jylee1429@users.noreply.github.com>
2023-10-17 02:25:59 +0900

    Add files via upload

--- Code.ino
+++ Code.ino
@@ -1,14 +1,16 @@
-#include "MotorControl.h"
-// #define SERIAL_PRINT 
-// #define TEST_LED
+#include "MotorControlSetting.h"
+//#define SERIAL_PRINT
 
-HX711 Loadcell;
-
-uint8_t status = FALSE;
-
+extern uint8_t state;
 // distance variable
-uint16_t distance;
-uint16_t filtered_distance;
+uint16_t distance = 0;
+uint16_t filtered_distance = 0;
+
+// frequency variable
+extern float frequency;
+extern uint8_t iteration;
+extern uint8_t ovf_cnt;
+extern uint32_t cnt;
 
 // current variable
 float cur_u;
@@ -17,79 +19,120 @@ float filtered_cur_u;
 float filtered_cur_v;
 
 // loadcell variable
-uint16_t scale_factor = 4782;
-float input_force = 0;
+extern float input_force;
 float filtered_input_force = 0;
+
+// contorller variable
+float i = 0;
+float prev_i = 0;
+float i_hat = 0;
+float w_hat = 0;
+float d_hat = 0;
+float prev_i_hat = 0;
+float prev_w_hat = 0;
+float prev_d_hat = 0;
+float u = 0;   
+float prev_u = 0;
+float err = 0; 
+float prev_err = 0; 
+float err_hat = 0;
+float prev_err_hat = 0;
+float pwm = 0;
+float input = 0;
+// PI controller gain
+float Kp = 10;
+float Ki = 850; //((Ra*Bm+Kt*Kb)/(Ra*Jm))*Kp;
 
 void setup(){
   Serial.begin(460800);
   GPIO_Setting();
+  FND_Setting();
+  LoadCell_Setting();
   PWM_Setting();
   Interrupt_Setting();
-  Loadcell.LoadCell_Setting(scale_factor);
-  
-  #ifdef SERIAL_PRINT
-  Serial.println("Setting finish");
-  #endif
+  Serial.println("setup finish");
 }
 
 void loop(){
 
 }
-// loadcell
-void TC6_Handler(){
-  REG_TC2_SR0;  // flag clear
-  input_force = Loadcell.Get_Input();
-  filtered_input_force = Loadcell.LPF_Input(filtered_input_force, 0.1); // theta = 0.1
-  
-  if(status == TRUE){
-    PWM_VALUE_RIGHT = input_force;
-    PWM_VALUE_LEFT = input_force;
-  }
 
-  #ifdef SERIAL_PRINT
-  Serial.print(input_force);
-  Serial.print("\t");
-  Serial.println(filtered_input_force);
-  #endif
+void TC3_Handler(){                   // overflow interrupt
+  REG_TC1_SR1;                        // flag clear
+  ovf_cnt++;
+}
+// Controller
+void TC4_Handler(){                   // 10ms
+  REG_TC1_SR1;                        // flag clear
+  // ref = Set_Input();
+  prev_i = i;
+  prev_i_hat = i_hat;
+  prev_w_hat = w_hat;
+  prev_d_hat = d_hat;
+
+  prev_err = err;
+  err = input_force - i;
+  prev_d_hat = d_hat;
+  d_hat = i - i_hat;                 // err_hat = current - current_hat;
+  prev_u = u;
+
+  // PI controller 
+  u = prev_u + ((Kp + (Ki * TIMEBASE / 2)) * err) + ((-Kp + (Ki * TIMEBASE / 2)) * prev_err) - ((L2 * TIMEBASE / 2) * (prev_err_hat + err_hat));
+  // u = prev_u + ((Kp + (Ki * timebase / 2)) * err) + ((-Kp + (Ki * timebase / 2)) * prev_err);
+  
+  // OBS 극점 -25, timestep=0.1ms
+  i_hat = (( -0.0686 * prev_i) + ( 0.9935 * prev_i_hat) + (-0.0019 * prev_w_hat) + (0.0623 * prev_d_hat)) + (0.0311 * (u + prev_u));
+  w_hat = (( 0.3017 * prev_i) + ( -0.0018 * prev_i_hat) + ( 0.9990 * prev_w_hat) + (-0.0001 * prev_d_hat)) + (0.0000 * (u + prev_u)); 
+  d_hat = (( 0.0003 * prev_i) + ( -0.0002 * prev_i_hat) + ( 0.0000 * prev_w_hat) + (1.0000 * prev_d_hat)) + (0.0000 * (u + prev_u)); 
+
+  // Saturation
+  if(u > 4.8)
+    u = 4.8;
+  else if(u < 0) // 반대방향으로 굴러가는 수레로 수정필요
+    u = 0;
+
+  pwm = u / 5;
+  input = TOP * pwm;
 }
 
-// current measure
-void TC7_Handler(){           // 10ms
-  REG_TC2_SR1;                // flag clear
-  // row data
-  cur_u = GetCurrent(ADC_PIN1, TIMES);
-  cur_v = GetCurrent(ADC_PIN2, TIMES);
-  // filtered data
-  filtered_cur_u = LPF_Current(filtered_cur_u, 0.1, ADC_PIN1);
-  filtered_cur_v = LPF_Current(filtered_cur_v, 0.1, ADC_PIN2);
+// FND
+void TC5_Handler(){                 // 10ms
+  REG_TC1_SR2;                      // flag clear
+  Display_FND(distance);
+}
 
-  #ifdef SERIAL_PRINT
-  digitalWrite(3, HIGH);   // test led 
-  Serial.print(cur_u);
-  Serial.print("\t");
-  Serial.println(cur_v);
-  Serial.print(filtered_cur_u);
-  Serial.print("\t");
-  Serial.println(filtered_cur_v);
-  #endif
+// loadcell
+void TC6_Handler(){                 // 100ms
+  REG_TC2_SR0;                      // flag clear
+  input_force = Get_Force();      
+  if(state == TRUE){                // system on
+    PWM_VALUE_RIGHT = input_force * 3000;
+    PWM_VALUE_LEFT = input_force * 3000;
+    // PWM_VALUE_RIGHT = input;
+    // PWM_VALUE_LEFT = input;
+  }
+  else{
+    System_Off();
+  }
+}
+
+// current measurement
+void TC7_Handler(){                  // 10ms
+  REG_TC2_SR1;                       // flag clear
+  // row data
+  // cur_u = GetCurrent(ADC_PIN1, TIMES);
+  // cur_v = GetCurrent(ADC_PIN2, TIMES);
+  // // filtered data
+  // filtered_cur_u = LPF_Current(filtered_cur_u, 0.1, ADC_PIN1);
+  // filtered_cur_v = LPF_Current(filtered_cur_v, 0.1, ADC_PIN2);
 }
 
 // ultrasonic sensor
-void TC8_Handler(){           // 100ms
-  REG_TC2_SR2;                // flag clear
-  // row data
-  distance = GetDistance();
-  // filtered data
-  filtered_distance = LPF_Distance(filtered_distance, 0.1); // theta = 0.1
-
-  if(filtered_distance < LIMIT_DISTANCE){
-    System_Off();
+void TC8_Handler(){                                         // 500ms
+  REG_TC2_SR2;                                              // flag clear
+  distance = GetDistance();                                 // row data
+  filtered_distance = LPF_Distance(filtered_distance, 0.1); // filtered data
+  if(distance < LIMIT_DISTANCE){                            // waring situlation
+    System_Off();                                           // system off
   }
-  
-  #ifdef SERIAL_PRINT
-  Serial.print(distance);
-  Serial.print("\t");
-  Serial.println(filtered_distance);
-  #endif
 }
--- MotorControlSetting.ino
+++ MotorControlSetting.ino
@@ -1,20 +1,28 @@
-#include "MotorControl.h"
+#include "MotorControlSetting.h"
+#include "Arduino.h"
+#include "MotorControlSetting.h"
+#include "Arduino.h"
 
-extern float input_force;
-extern uint8_t status;
+float scale_factor = 4782;
+extern float input_force = 0;
+extern float frequency = 0;
+extern uint8_t state = FALSE;
+extern uint8_t iteration = 0;
+extern uint8_t ovf_cnt = 0;
+extern uint32_t cnt = 0;
+
+LoadCell loadcell;
+FND fnd;
+
 
 void System_On(void){
-  status = TRUE; 
-  #ifdef SERIAL_PRINT
-  Serial.println("system on");
-  #endif
+  state = TRUE; 
+  digitalWrite(LED_PIN, HIGH);          // led on
 }
 
 void System_Off(void){
-  #ifdef SERIAL_PRINT
-  Serial.println("system off");
-  #endif
-  status = FALSE;
+  state = FALSE;
+  digitalWrite(LED_PIN, LOW);           // led off
   PWM_VALUE_RIGHT = 0;
   PWM_VALUE_LEFT = 0;
 }
@@ -22,39 +30,78 @@ void System_Off(void){
 // Motor On/Off function
 void Motor_OnOff_Switch(void){
   uint8_t sw_state = digitalRead(SW_PIN);
-  // switch On
-  if(sw_state == HIGH){
-    delayMicroseconds(10000);
-    if(sw_state == HIGH){
-      System_On();
-      #ifndef TEST_LED
-      digitalWrite(3, LOW);
-      #endif
-    }
+  if(sw_state == HIGH){                   // switch Off
+    System_Off();                         // if switch off, system off
   }
-  // switch Off
-  else{
-    delayMicroseconds(10000); // delay 10ms
-    if(sw_state == LOW){
-      System_Off();
-      #ifndef TEST_LED
-      digitalWrite(3, HIGH);
-      #endif
-    }
+  else{                                   // switch On
+    System_On();                         // if switch on, system on
   }
 }
+void FND_Setting(void){
+  SPI_Init();
+  FND_Init(&fnd, RCLK_PIN);
+  
+  #ifdef SERIAL_PRINT
+  Serial.println("Loadcell setting");
+  #endif
+}
 
+void Display_FND(int val){
+  digitalWrite(SS, LOW);
+  Digit4_Zero(&fnd, val, 100);
+}
+
+void LoadCell_Setting(void){
+  Init(&loadcell, DOUT_PIN, CLK_PIN);
+  Tare(&loadcell, TIMES);
+  Set_Scale(&loadcell, scale_factor);
+
+  #ifdef SERIAL_PRINT
+  Serial.println("Loadcell setting");
+  #endif
+}
+
+float Get_Force(void){
+  return Get_Units(&loadcell, 1) * 0.453592; // [kg]
+}
+
+void Get_Frequency(void){
+  uint32_t temp, time;
+
+  if(iteration== FALSE){
+    cnt = REG_TC0_CV0;
+    ovf_cnt = 0;
+  }
+  else{
+    temp = REG_TC0_CV0;
+    time = ((ovf_cnt * 65536) + temp) - cnt;
+    frequency = 1 / time;
+    cnt = temp;
+    ovf_cnt = 0;
+  }
+  iteration = TRUE;
+}
+
+uint16_t Get_Velocity(float freq){
+  uint16_t vel;
+  vel = freq / POLE * 60;
+  return vel;
+}
 
 void GPIO_Setting(void){
   pinMode(SW_PIN, INPUT);                       // On/Off switch
   pinMode(LED_PIN, OUTPUT);                     // test LED 
   pinMode(TRIGGER_PIN, OUTPUT);                 // ultrasonic sensor-trigger pin
-  pinMode(ECHO_PIN, INPUT);                     // ultrasonic sensor-echo pin
-  pinMode(SCLK_PIN, OUTPUT);
-  pinMode(DOUT_PIN, INPUT);
-  pinMode(DIR, OUTPUT);
-  #ifdef SERIAL_PRINT 
-  Serial.println("GPIO Setting");
+  pinMode(ECHO_PIN, INPUT);                     // ultrasonic sensor-echo pin         
+  pinMode(FG_PIN, INPUT);                   
+  
+  // Debouncing filter
+  REG_PIOB_IFER |= 1 << 25;
+  REG_PIOB_DIFSR |= 1 << 25;
+  REG_PIOB_SCDR |= 0xFF;                        // 16 ms
+
+  #ifdef SERIAL_PRINT
+  Serial.println("GPIO setting");
   #endif
 }
 
@@ -70,7 +117,6 @@ void PWM_Setting(void){
   REG_PWM_CPRD2 = TOP;                                // TOP value
   REG_PWM_CDTY2 = PWM_VALUE_RIGHT;                    // OC value
   REG_PWM_ENA = PWM_ENA_CHID2;                        // channel2 PWM enable
-  
   // PWM channel 3
   // PWML3(PC8)
   REG_PWM_DIS = PWM_DIS_CHID3;                        // Disable PWM channel 3
@@ -80,23 +126,50 @@ void PWM_Setting(void){
   REG_PWM_CPRD3 = TOP;                                // TOP value
   REG_PWM_CDTY3 = PWM_VALUE_LEFT;                     // OC value
   REG_PWM_ENA = PWM_ENA_CHID3;                        // channel2 PWM enable
-  #ifdef SERIAL_PRINT 
-  Serial.println("PWM Setting");
+  #ifdef SERIAL_PRINT
+  Serial.println("PWM setting");
   #endif
 }
 
 void Interrupt_Setting(void){
   // PMC Peripheral Clock Enable Register0
-  REG_PMC_PCER1 |= PMC_PCER1_PID33;               // ID-34 TC2 channel(TC6)
+  REG_PMC_PCER0 |= PMC_PCER0_PID30;               // ID-30 TC1 channel(TC3)
+  REG_PMC_PCER0 |= PMC_PCER0_PID31;               // ID-31 TC1 channel(TC4)
+  REG_PMC_PCER1 |= PMC_PCER1_PID32;               // ID-32 TC1 channel(TC5)
+  REG_PMC_PCER1 |= PMC_PCER1_PID33;               // ID-33 TC2 channel(TC6)
   REG_PMC_PCER1 |= PMC_PCER1_PID34;               // ID-34 TC2 channel(TC7)
   REG_PMC_PCER1 |= PMC_PCER1_PID35;               // ID-35 TC2 channel(TC8)
+
+  // Overflow interrupt - Ch3
+  REG_TC1_CMR0 = TC_CMR_TCCLKS_TIMER_CLOCK1       // Internal MCK/128 clock signal
+               | TC_CMR_WAVE                      // waveform mode
+               | TC_CMR_ACPC_TOGGLE               // toggle TIOA output
+               | TC_CMR_WAVSEL_UP_RC;             // UP mode with automatic trigger
+  REG_TC1_RC0 = 65536;                            // TOP value                    
+  REG_TC1_IER0 = TC_IER_CPCS;                     // overflow interrupt enable
+
+  // OC interrupt(1ms) - Ch4
+  REG_TC1_CMR1 = TC_CMR_TCCLKS_TIMER_CLOCK1       // Internal MCK/2 clock signal
+               | TC_CMR_WAVE                      // waveform mode
+               | TC_CMR_ACPC_TOGGLE               // toggle TIOA output
+               | TC_CMR_WAVSEL_UP_RC;             // up count until count = RC     
+  REG_TC1_RC1 = VAL_1MS;                          // set RC2 value
+  REG_TC1_IER1 = TC_IER_CPCS;                     // RC compare interrupt enable
   
-  // OC interrupt(100ms) - Ch7
+  // OC interrupt(10ms) - Ch5
+  REG_TC1_CMR2 = TC_CMR_TCCLKS_TIMER_CLOCK1       // Internal MCK/2 clock signal
+               | TC_CMR_WAVE                      // waveform mode
+               | TC_CMR_ACPC_TOGGLE               // toggle TIOA output
+               | TC_CMR_WAVSEL_UP_RC;             // up count until count = RC     
+  REG_TC1_RC2 = VAL_10MS;                         // set RC2 value
+  REG_TC1_IER2 = TC_IER_CPCS;                     // RC compare interrupt enable
+  
+  // OC interrupt(100ms) - Ch6
   REG_TC2_CMR0 = TC_CMR_TCCLKS_TIMER_CLOCK1       // Internal MCK/2 clock signal
                | TC_CMR_WAVE                      // waveform mode
                | TC_CMR_ACPC_TOGGLE               // toggle TIOA output
                | TC_CMR_WAVSEL_UP_RC;             // up count until count = RC     
-  REG_TC2_RC0 = VAL_100MS;                         // set RC2 value
+  REG_TC2_RC0 = VAL_50MS;                         // set RC2 value
   REG_TC2_IER0 = TC_IER_CPCS;                     // RC compare interrupt enable
   
   // OC interrupt(10ms) - Ch7
@@ -112,21 +185,35 @@ void Interrupt_Setting(void){
                | TC_CMR_WAVE                      // waveform mode
                | TC_CMR_ACPC_TOGGLE               // toggle TIOA output
                | TC_CMR_WAVSEL_UP_RC;             // up count until count = RC     
-  REG_TC2_RC2 = VAL_100MS;                        // set RC2 value
+  REG_TC2_RC2 = VAL_50MS;                         // set RC2 value
   REG_TC2_IER2 = TC_IER_CPCS;                     // RC compare interrupt enable
-
+  
+  NVIC_EnableIRQ(TC3_IRQn); 
+  NVIC_EnableIRQ(TC4_IRQn); 
+  NVIC_EnableIRQ(TC5_IRQn); 
   NVIC_EnableIRQ(TC6_IRQn); 
   NVIC_EnableIRQ(TC7_IRQn); 
   NVIC_EnableIRQ(TC8_IRQn);
 
+  /// setting interrupt priority
+  NVIC_SetPriority(TC4_IRQn, 0);                   // controller
+  NVIC_SetPriority(TC5_IRQn, 3);                   // FND
+  NVIC_SetPriority(TC6_IRQn, 2);                   // loadcell
+  NVIC_SetPriority(TC7_IRQn, 1);                   // current measurement
+  NVIC_SetPriority(TC8_IRQn, 3);                   // Ultrasonic sensor
+
+  REG_TC1_CCR0 = TC_CCR_CLKEN | TC_CCR_SWTRG;
+  REG_TC1_CCR1 = TC_CCR_CLKEN | TC_CCR_SWTRG;
+  REG_TC1_CCR2 = TC_CCR_CLKEN | TC_CCR_SWTRG;
   REG_TC2_CCR0 = TC_CCR_CLKEN | TC_CCR_SWTRG;
   REG_TC2_CCR1 = TC_CCR_CLKEN | TC_CCR_SWTRG;
   REG_TC2_CCR2 = TC_CCR_CLKEN | TC_CCR_SWTRG;
 
   // Externel Interrut(PB25 - pin2)
   attachInterrupt(SW_PIN, Motor_OnOff_Switch, CHANGE);
+  attachInterrupt(2, Get_Frequency, RISING);
 
-  #ifdef SERIAL_PRINT 
-  Serial.println("Interrupt Setting");
+  #ifdef SERIAL_PRINT
+  Serial.println("Ineterrupt setting");
   #endif
 }
